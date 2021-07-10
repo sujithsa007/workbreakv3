63962710dc8df8cecd83efdb0b18d9a1e753e454
sujithsa007 <sujithsa007@gmail.com>
2021-07-10 19:03:08 +0530

    ad helper bug fix

--- lib/utilities/ad_helper.dart
+++ lib/utilities/ad_helper.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3470431594248807~2331378930';
+      return 'ca-app-pub-3940256099942544/6300978111';
     }
     // else if (Platform.isIOS) {
     //   return 'ca-app-pub-3940256099942544/2934735716';
@@ -11,5 +11,12 @@ class AdHelper {
     throw new UnsupportedError("Unsupported platform");
   }
 
-//
-
+  // static String get nativeAdUnitId {
+  //   if (Platform.isAndroid) {
+  //     return 'ca-app-pub-3940256099942544/2247696110';
+  //   } else if (Platform.isIOS) {
+  //     return 'ca-app-pub-3940256099942544/3986624511';
+  //   }
+  //   throw new UnsupportedError("Unsupported platform");
+  // }
+}
