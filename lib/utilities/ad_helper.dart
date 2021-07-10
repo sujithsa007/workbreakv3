b0944c8278b775b9925dec39f62605abb0db5fa7
sujithsa007 <sujithsa007@gmail.com>
2021-07-10 19:04:56 +0530

    ad helper App Id

--- lib/utilities/ad_helper.dart
+++ lib/utilities/ad_helper.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-3470431594248807~2331378930';
     }
     // else if (Platform.isIOS) {
     //   return 'ca-app-pub-3940256099942544/2934735716';
