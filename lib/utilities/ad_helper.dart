aad1680201832622ac540d341cd5f53411b81bbb
sujithsa007 <sujithsa007@gmail.com>
2021-07-10 18:53:14 +0530

    app id updated

--- lib/utilities/ad_helper.dart
+++ lib/utilities/ad_helper.dart
@@ -3,21 +3,13 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-3470431594248807/1419706225";
-    } else if (Platform.isIOS) {
-      return "<YOUR_IOS_BANNER_AD_UNIT_ID>";
-    } else {
-      throw new UnsupportedError("Unsupported platform");
+      return 'ca-app-pub-3470431594248807~2331378930';
     }
+    // else if (Platform.isIOS) {
+    //   return 'ca-app-pub-3940256099942544/2934735716';
+    // }
+    throw new UnsupportedError("Unsupported platform");
   }
 
-  static String get nativeAdUnitId {
-    if (Platform.isAndroid) {
-      return "<YOUR_ANDROID_NATIVE_AD_UNIT_ID>";
-    } else if (Platform.isIOS) {
-      return "<YOUR_IOS_NATIVE_AD_UNIT_ID>";
-    } else {
-      throw new UnsupportedError("Unsupported platform");
-    }
-  }
-}
+//
+
